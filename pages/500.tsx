11cb04d0166412d2beac1860dec6fa1999bda058
idm1try <dimasavchenko1706@icloud.com>
2022-09-15 14:52:23 +0500

    chore: add 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,21 @@
+import { Box, Heading, Text, useColorModeValue } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <Box textAlign='center' mt={5}>
+      <Heading
+        display='inline-block'
+        as='h2'
+        size='2xl'
+        color={useColorModeValue('teal.500', 'teal.200')}
+      >
+        500
+      </Heading>
+      <Text fontSize='18px' fontWeight='bold' mt={3} mb={5}>
+        Internal Server Error
+      </Text>
+    </Box>
+  );
+};
+
+export default NotFound;
