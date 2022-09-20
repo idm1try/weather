48c28924346e3a48600c0d857d13105644aa61b1
idm1try <dimasavchenko1706@icloud.com>
2022-09-20 20:55:18 +0500

    chore: update deps

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,10 @@ import { Box } from '@chakra-ui/react';
 const Footer = () => {
   return (
     <Box textAlign='center' color='gray.500' fontSize='sm' mt={5}>
-      &copy; {new Date().getFullYear()} <b>idm1try</b>
+      &copy; {new Date().getFullYear()}{' '}
+      <a href='https://idm1try.github.io'>
+        <b>idm1try</b>
+      </a>
     </Box>
   );
 };
