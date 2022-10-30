8e16f7092c99c9c8046daa7dd391b77eff91c082
idm1try <dimasavchenko1706@icloud.com>
2022-10-30 11:55:39 +0500

    revert: weather api route

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,77 +1,32 @@
-import { WeatherResponse } from 'lib/types';
-import { NextRequest } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export const config = {
-  runtime: 'experimental-edge',
-};
-
-export default async function weather(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-
-  const unsanitizedLocation = searchParams.get('location') || '';
-  const unsanitizedData = searchParams.get('data') || '';
-
-  const location = encodeURI(unsanitizedLocation);
-  const data = encodeURI(unsanitizedData);
-
-  if (!location) {
-    return new Response(JSON.stringify({ error: 'No location provided.' }), {
-      status: 400,
-      statusText: 'Bad Request',
+async function weather(req: NextApiRequest, res: NextApiResponse) {
+  if (!req.query.location) {
+    return res.status(400).json({
+      message: 'Cannot fetch weather information without a location query.',
     });
   }
 
-  if (!data) {
-    return new Response(JSON.stringify({ error: 'No data type provided.' }), {
-      status: 400,
-      statusText: 'Bad Request',
+  if (!req.query.data) {
+    return res.status(400).json({
+      message: 'Cannot fetch weather information without a data query.',
     });
   }
 
   try {
-    const baseUrl = `https://api.openweathermap.org/data/2.5/${data}?q=${location}&units=metric&cnt=8&appid=${process.env.WEATHER_API_KEY}`;
+    const baseUrl = `https://api.openweathermap.org/data/2.5/${req?.query?.data}?q=${req?.query?.location}&units=metric&cnt=8&appid=${process.env.WEATHER_API_KEY}`;
 
     const weather = await fetch(baseUrl);
+    const forecast = await weather.json();
 
-    if (weather.status != 200) {
-      return new Response(
-        JSON.stringify({
-          error: `${weather.statusText}`,
-        }),
-        {
-          status: weather.status,
-          statusText: weather.statusText,
-        }
-      );
+    if (Object.keys(forecast).length) {
+      return res.status(200).json(forecast);
+    } else {
+      return res.status(500).json({ message: 'Cannot fetch weather data.' });
     }
-
-    const forecast = (await weather.json()) as WeatherResponse;
-
-    if (!forecast) {
-      return new Response(
-        JSON.stringify({
-          error: 'No forecast data.',
-        }),
-        {
-          status: 400,
-          statusText: 'Bad Request',
-        }
-      );
-    }
-
-    return new Response(JSON.stringify(forecast), {
-      headers: {
-        'Content-Type': 'application/json',
-        'Cache-Control': 'public, s-maxage=1, stale-while-revalidate=59',
-      },
-      status: 200,
-      statusText: 'OK',
-    });
   } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: `${error}` }), {
-      status: 500,
-      statusText: 'Internal Server Error',
-    });
+    return res.status(500).json({ message: error });
   }
 }
+
+export default weather;
